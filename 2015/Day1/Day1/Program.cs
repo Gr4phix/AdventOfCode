2511c52b2d7ba8b7b3c63421cd1a9ad3ad209f05
FL0RI <florian.schuster@mailbox.tu-dresden.de>
2017-12-03 21:54:58 +0100

    Day1 2015 added

---
+++ 2015/Day1/Day1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stream inputStream = Console.OpenStandardInput(10000);
+            Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, 10000));
+            String input = Console.ReadLine();
+            char[] puzzle = input.ToCharArray();
+
+            int floor = 0;
+            bool secondPart = true;
+
+            for (int i = 0; i < puzzle.Length; i++)
+            {
+                if (puzzle[i] == '(') floor++;
+                else floor--;
+
+                if (floor == -1 && secondPart)
+                {
+                    Console.Write("He enterd the basement in character {0}. \n ", i + 1);
+                    secondPart = false;
+                }
+            }
+
+            Console.Write("He's now in floor {0}. ", floor);
+            
+            Console.ReadKey();
+        }
+    }
+}
