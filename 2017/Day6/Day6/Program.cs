fa8fea3f59a5a8d8c38f5f977bba40286e90efac
FL0RI <florian.schuster@mailbox.tu-dresden.de>
2017-12-09 22:17:56 +0100

    Day6 passed

--- 2017/Day6/Day6/Program.cs
+++ 2017/Day6/Day6/Program.cs
@@ -16,6 +16,7 @@ namespace Day6
             string[] blocksArray = line.Split(new Char[] { ' ' });
 
             List<int> blocks = new List<int>();
+            List<int> help2 = new List<int>();
             for (int i = 0; i < blocksArray.Length; i++)
             {
                 blocks.Add(Convert.ToInt32(blocksArray[i]));
@@ -23,44 +24,58 @@ namespace Day6
 
             int length = blocks.Count;
             List<List<int>> help = new List<List<int>>();
-            bool finish = false;
+            bool finish = false, finish2 = false;
 
-            int count = 0;
+            int count = 0, count2 = 0;
+
             do
             {
                 int max = blocks.Max();
                 int index = blocks.IndexOf(max);
 
-                blocks[index] = 0;             
+                blocks[index] = 0;
 
-                for (int i = ((index+=1) >= length ? 0 : index++); max > 0; max--)
-                {                                     
+                for (int i = ((index += 1) >= length ? 0 : index++); max > 0; max--)
+                {
                     blocks[i] = blocks[i] + 1;
 
                     i++;
                     if (i >= length) i = 0;
 
-                    Console.WriteLine("{0} : {1} --- {2}", max, String.Join(", ", blocks), count);
-                    Console.ReadKey();
+                    //Console.WriteLine("{0} : {1} --- {2}", max, String.Join(", ", blocks), count);
+                    //Console.ReadKey();
                 }
-                Console.WriteLine("{0} : {1} --- {2}", 0, String.Join(", ", blocks), count);
-                Console.ReadKey();
+                //Console.WriteLine("{0} : {1} --- {2}", 0, String.Join(", ", blocks), count);
+                //Console.ReadKey();
                 count++;
 
+                if(!finish)
                 foreach (List<int> lists in help)
                 {
-                    if(blocks.SequenceEqual(lists))
+                    if (blocks.SequenceEqual(lists))
                     {
                         finish = true;
+                        help2 = blocks.ToList();
+
+                        goto hmm;
                     }
                 }
 
-                if(!finish)
-                help.Add(blocks);
+                if (finish)
+                    if (!blocks.SequenceEqual(help2))
+                        count2++;
+                    else finish2 = true;
 
-            } while (!finish);
+                hmm:
 
-            Console.Write("#1: {0} redistribution cycles needed. ", count);
+                if (!finish)
+                    help.Add(blocks.ToList());
+
+            } while (!finish2);
+
+            Console.Write("#1: {0} redistribution cycles needed. \n", count - count2 -1);
+            Console.Write("#2: {0} redistribution cycles needed. ", count2 +1);
+
 
             Console.ReadKey();
         }
