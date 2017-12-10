a5728a63d29bd37dd9bffe827fb810dbe66d0b8c
FL0RI <florian.schuster@mailbox.tu-dresden.de>
2017-12-10 02:05:09 +0100

    Day7 part 2 not working yet

--- 2017/Day7/Day7/Program.cs
+++ 2017/Day7/Day7/Program.cs
@@ -1,44 +1,57 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day7
 {
+    public class Node
+    {
+        public string Name { get; set; }
+        public int Weight { get; }
+        public List<string> Children { get; }
+
+        public Node(string name, int weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.Children = new List<string>();
+        }
+
+        public Node(string name, int weight, IEnumerable<string> children)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.Children = new List<string>();
+
+            foreach (string child in children)
+            {
+                Children.Add(child);
+            }
+        }
+    }
+
     class Program
     {
-        public class Node
+        public static Dictionary<string, int> progs = new Dictionary<string, int>();
+        public static List<Node> nodes = new List<Node>();
+
+        public static int GetTotalWeight(IEnumerable childNames)
         {
-            public string Name { get; set; }
-            public int Weight { get; }
-            public List<string> Children { get; }
-
-            public Node(string name, int weight)
-            {
-                this.Name = name;
-                this.Weight = weight;
-            }
-
-            public Node(string name, int weight, IEnumerable<string> children)
-            {
-                this.Name = name;
-                this.Weight = weight;
-                this.Children = new List<string>();
-
-                foreach (string child in children)
-                {
-                    Children.Add(child);
-                }
-            }
+            //Console.WriteLine("{0}", String.Join(", ", childNames.ToString()));
+            foreach (string childName in childNames)
+                foreach (Node node in nodes)
+                    if (node.Name.Equals(childName))
+                        if (node.Children.Count > 0)
+                            return (node.Weight + GetTotalWeight(node.Children));
+                        else return node.Weight;
+            return 0;
         }
 
         static void Main(string[] args)
         {
             List<String> input = File.ReadLines(@"input.txt").ToList();
-            Dictionary<string, int> progs = new Dictionary<string, int>();
-            List<Node> nodes = new List<Node>();
 
             foreach (String inputString in input)
             {
@@ -52,8 +65,8 @@ namespace Day7
                     helpW += inputChar[i];
 
                 progs.Add(helpN, Convert.ToInt32(helpW));
-               
-                if (inputChar.Count() > Array.IndexOf(inputChar, ')') +1 )
+
+                if (inputChar.Count() > Array.IndexOf(inputChar, ')') + 1)
                 {
                     int childsCount = 0;
                     string childString = "";
@@ -101,8 +114,25 @@ namespace Day7
                     }
                 }
                 catch { }
+            string nameFirst = "";
 
-            Console.Write("#1: {0} ", String.Join(", ", helpNames));                 
+            foreach (string name in helpNames)
+                nameFirst += name;
+
+            Console.Write("#1: {0} \n", /*String.Join(", ", helpNames)*/ nameFirst.ToString());
+
+            Dictionary<string, int> balanced = new Dictionary<string, int>();
+
+            foreach (Node node in nodes)
+                if (node.Name.Equals(nameFirst))
+                    foreach (string child in node.Children)
+                        foreach (Node node2 in nodes)
+                            if (node2.Name.Equals(child))
+                                balanced.Add(node2.Name, GetTotalWeight(node2.Children));
+
+            Console.Write("#2: {0} ", String.Join("; ", balanced.Select(x => x.Key + "=" + x.Value).ToArray()));
+            Console.Write("Total-total sum: {0}. \n", progs.Values.Sum());
+
             Console.ReadKey();
         }
     }
