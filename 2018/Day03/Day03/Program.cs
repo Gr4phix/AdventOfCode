5a17e2ac607d88551aff49d3d984ae9130e2ca22
florian <florian.schuster6@gmx.de>
2018-12-03 16:20:00 +0100

    Day 3 finished

--- 2018/Day03/Day03/Program.cs
+++ 2018/Day03/Day03/Program.cs
@@ -11,8 +11,7 @@ namespace Day03
         {
             List<string> inputStrings = File.ReadLines(Directory.GetCurrentDirectory() + @"/input").ToList();
 
-            int[,] fieldArray = new int[10000, 10000];
-
+            int[,] fieldArray = new int[5000, 5000];
 
             foreach (var patch in inputStrings)
             {
@@ -22,9 +21,9 @@ namespace Day03
                     var start = temp.Split(':')[0].Trim();
                     var size = temp.Split(':')[1].Trim();
 
-                    for (int row = Convert.ToInt32(start.Split(',')[1].Trim()); row < Convert.ToInt32(size.Split('x')[1].Trim()); row++)
+                    for (int row = Convert.ToInt32(start.Split(',')[1].Trim()); row < Convert.ToInt32(size.Split('x')[1].Trim()) + Convert.ToInt32(start.Split(',')[1].Trim()); row++)
                     {
-                        for (int column = Convert.ToInt32(start.Split(',')[0].Trim()); column < Convert.ToInt32(size.Split('x')[0].Trim()); column++)
+                        for (int column = Convert.ToInt32(start.Split(',')[0].Trim()); column < Convert.ToInt32(size.Split('x')[0].Trim()) + Convert.ToInt32(start.Split(',')[0].Trim()); column++)
                         {
                             fieldArray[row, column] += 1;
                         }
@@ -33,15 +32,44 @@ namespace Day03
             }
 
             var count = 0;
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < fieldArray.GetLength(0); i++)
             {
-                for (int j = 0; j < 10000; j++)
+                for (int j = 0; j < fieldArray.GetLength(1); j++)
                 {
                     if (fieldArray[i, j] > 1)
                         count++;
                 }
             }
             Console.WriteLine($"{count} square inches of fabric are within two or more claims.");
+
+
+            //Part two:
+            foreach (var patch in inputStrings)
+            {
+                if (patch != "")
+                {
+                    var temp = patch.Split('@')[1].Trim();
+                    var start = temp.Split(':')[0].Trim();
+                    var size = temp.Split(':')[1].Trim();
+
+                    var overlapping = false;
+
+                    for (int row = Convert.ToInt32(start.Split(',')[1].Trim()); row < Convert.ToInt32(size.Split('x')[1].Trim()) + Convert.ToInt32(start.Split(',')[1].Trim()); row++)
+                    {
+                        for (int column = Convert.ToInt32(start.Split(',')[0].Trim()); column < Convert.ToInt32(size.Split('x')[0].Trim()) + Convert.ToInt32(start.Split(',')[0].Trim()); column++)
+                        {
+                            if (fieldArray[row, column] > 1)
+                                overlapping = true;
+                        }
+                    }
+
+                    if(!overlapping)
+                    {
+                        Console.WriteLine($"{patch.Split('@')[0].Trim()} the ID of the only claim that doesn't overlap.");
+                        return;
+                    }
+                }
+            }
         }
     }
 }
